b4b03d55dcccadd7fe7c7f724b78d9e09d56ef22
Jesse De Meulemeester <jesse.demeulemeester@student.kuleuven.be>
2022-01-02 12:22:22 +0000

    Added examples for ARM

---
+++ examples/aarch64/v1_various_cond_cycles/cond_uncached.c
@@ -0,0 +1,53 @@
+#include <linux/perf_event.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+
+#define NOP asm volatile("nop");
+#define NOP2 NOP NOP
+#define NOP4 NOP2 NOP2
+#define NOP8 NOP4 NOP4
+#define NOP16 NOP8 NOP8
+#define NOP32 NOP16 NOP16
+#define NOP64 NOP32 NOP32
+#define NOP128 NOP64 NOP64
+
+int main(int argc, char *argv[], char *envp[]) {
+  int fd = atoi(getenv("group_fd"));
+
+  int warmup_cnt = 0;
+
+  // ##### DATA STARTS HERE #####
+
+
+  // #####  DATA ENDS HERE  #####
+
+loop:
+  ioctl(fd, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP);
+  ioctl(fd, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP);
+
+  asm volatile("DC CIVAC, %0" : : "r"(&warmup_cnt));
+  asm volatile("DSB ISH");
+
+  NOP128
+
+  if (warmup_cnt == 12)
+    goto skip;
+
+  // ##### SNIPPET STARTS HERE #####
+
+
+  // #####  SNIPPET ENDS HERE  #####  
+
+  asm volatile("fmov s1, #1.00000000");  // Marker
+
+skip:
+  NOP128
+
+  ioctl(fd, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP);
+
+  if (++warmup_cnt < 13)
+    goto loop;
+
+end:
+  return 0;
+}
